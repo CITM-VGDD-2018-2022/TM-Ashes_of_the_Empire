c2480a7fafa9e43a028ae860cfff284a47b10d29
Marc Pagès Francesch <47534017+marcpages2020@users.noreply.github.com>
2021-06-01 09:33:46 +0200

    [CODE] Height check to avoid infinite fall and idling mando

--- Diamond Engine/Project Folder/Assets/Scripts/Respawn.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/Respawn.cs
@@ -3,9 +3,6 @@ using DiamondEngine;
 
 public class Respawn : DiamondComponent
 {
-	//public GameObject spawnpoint = null;
-	//public GameObject player = null;
-    public int room;
     bool haveToRespawn = false; //I don't know why but it doesn't edit mando's pos on collision enter we have to have a dirty bool and do it from the update
     public void OnCollisionEnter(GameObject collidedGameObject)
     {
@@ -18,6 +15,11 @@ public class Respawn : DiamondComponent
 
     public void Update()
 	{
+        if(Core.instance != null && Core.instance.gameObject.transform.globalPosition.y < -20.0f)
+        {
+            haveToRespawn = true;
+        }
+
         if(haveToRespawn)
         {
             Core.instance.gameObject.GetComponent<Core>().RespawnOnFall();
