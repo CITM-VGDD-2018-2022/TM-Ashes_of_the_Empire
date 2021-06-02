540dddcb7f2f0a3bd14fbdb50ebda51504be08b2
Jordi Pardo <jordipardogutierrez@gmail.com>
2021-06-02 23:37:59 +0200

    [CODE] Working on cinematic

--- Diamond Engine/Project Folder/Assets/Scripts/CinematicManager.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/CinematicManager.cs
@@ -3,16 +3,40 @@ using DiamondEngine;
 
 public class CinematicManager : DiamondComponent
 {
-    private GameObject gameCamera;
+    public GameObject gameCamera;
     public GameObject cameraPos1;
+    public GameObject cameraPos2;
+    private Vector3 initPos;
+    private Quaternion initRot;
 
     public void Awake()
     {
-        gameCamera = InternalCalls.FindObjectWithName("Game Camera");
+        initPos = gameCamera.transform.localPosition;
+        initRot = gameCamera.transform.localRotation;
+        Debug.Log(gameCamera.transform.localPosition.ToString());
+        Debug.Log(cameraPos1.transform.localPosition.ToString());
+        SetAsPerspectiveCamera();
 
-        if (cameraPos1 != null)
-        {
-            gameCamera.transform = cameraPos1.transform;
-        }
+        gameCamera.transform.localPosition = cameraPos1.transform.localPosition;
+        gameCamera.transform.localRotation = cameraPos1.transform.localRotation;
+    }
+
+    public void Update()
+    {
+
+            //ResetInitalTransform();
+        
+    }
+    private void SetAsPerspectiveCamera()
+    {
+        CameraManager.SetCameraPerspective(gameCamera);
+        CameraManager.SetVerticalFOV(gameCamera, 60.0f);
+    }
+
+    private void ResetInitalTransform()
+    {
+        gameCamera.transform.localPosition = initPos;
+        gameCamera.transform.localRotation = initRot;
+        CameraManager.SetCameraOrthographic(gameCamera);
     }
 }
