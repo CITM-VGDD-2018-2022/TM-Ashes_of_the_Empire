e3f54a80a4e43ab60fa0f866ac06458a4dec072a
Miquel Suau Gonzàlez <sg.miquel@gmail.com>
2021-06-03 20:17:51 +0200

    Update IM_TextureImporter.cpp

--- Diamond Engine/Source Code/IM_TextureImporter.cpp
+++ Diamond Engine/Source Code/IM_TextureImporter.cpp
@@ -158,7 +158,7 @@ void TextureImporter::LoadCubeMap(char* faces[6], DE_Cubemap& cubeMap)
 		{
 			glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
-				0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data
+				0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data
 			);
 
 			//glTexParameterf(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
