6b49afdd536e730e4e2b86be9b6ccc651d686873
Marc Pagès Francesch <47534017+marcpages2020@users.noreply.github.com>
2021-06-11 12:02:32 +0200

    [CODE] Timer added to LevelEnd and skytrooper trajectories fixed

--- Diamond Engine/Project Folder/Assets/Scripts/Skel.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/Skel.cs
@@ -602,12 +602,4 @@ public class Skel : Bosseslv2
         speed = 8.0f;
         //SetJumpValues(0.5f, 0.3f, 1.0f, 0.4f);
     }
-
-
-
-    public void OnDestroy()
-    {
-        EnemyManager.RemoveEnemy(this.gameObject);
-    }
-
 }
