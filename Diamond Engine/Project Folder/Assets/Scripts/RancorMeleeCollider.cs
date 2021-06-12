27fa6bea3657259d21c2e01a95fb02d22f36d990
jose-tello <47975174+jose-tello@users.noreply.github.com>
2021-06-12 11:27:59 +0200

    [SCRIPTING] Rancor melee attack does damage

---
+++ Diamond Engine/Project Folder/Assets/Scripts/RancorMeleeCollider.cs
@@ -0,0 +1,35 @@
+using System;
+using DiamondEngine;
+
+public class RancorMeleeCollider : DiamondComponent
+{
+    public float colliderSpeed = 1.0f;
+    public float lifeTime = 2.0f;
+
+    public int damage = 10;
+
+
+    public void Update()
+    {
+        if (lifeTime > 0.0f)
+        {
+            lifeTime -= Time.deltaTime;
+
+            if (lifeTime <= 0.0f)
+                InternalCalls.Destroy(gameObject);
+        }
+
+        gameObject.transform.localPosition += gameObject.transform.GetForward() * colliderSpeed * Time.deltaTime;
+    }
+
+    public void OnTriggerEnter(GameObject triggeredGameObject)
+    {
+        if (triggeredGameObject.CompareTag("Player"))
+        {
+            PlayerHealth health = triggeredGameObject.GetComponent<PlayerHealth>();
+            if (health != null)
+                health.TakeDamage(damage);
+        }
+    }
+
+}
