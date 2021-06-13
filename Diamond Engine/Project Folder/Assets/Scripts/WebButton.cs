874e42b6cb0f3d894ad7ca1a428df56e598f818b
jose-tello <47975174+jose-tello@users.noreply.github.com>
2021-06-13 13:04:43 +0200

    web button added to main menu

---
+++ Diamond Engine/Project Folder/Assets/Scripts/WebButton.cs
@@ -0,0 +1,13 @@
+using System;
+using DiamondEngine;
+
+public class WebButton : DiamondComponent
+{
+    public void OnExecuteButton()
+    {
+
+        System.Diagnostics.Process.Start("https://citm-vgdd-2018-2022.github.io/TM-Ashes_of_the_Empire/");
+
+    }
+
+}
